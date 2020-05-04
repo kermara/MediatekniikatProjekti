199ead097270e8561b430ff44a16379f99b364fd
IlmastMaksim <ilmastm9@gmail.com>
2020-05-04 17:00:41 +0300

    2 charts added, header design done and  code polishing

--- js/chart.js
+++ js/chart.js
@@ -1,74 +1,82 @@
-const getTwoMonthAgoDate = () => {
-    const todaysDate = new Date();
-    let twoMonthAgoDate = new Date(todaysDate.valueOf());
-    const twoMonthAgoMonth = todaysDate.getMonth() - 2;
-    twoMonthAgoDate.setMonth(twoMonthAgoMonth)
-    const twoMonthAgoDateFormatted = twoMonthAgoDate.getFullYear() + '-' +  ('0' + (twoMonthAgoDate.getMonth() +1 )).slice(-2) + '-' + ('0' + twoMonthAgoDate.getDate()).slice(-2);
-    return twoMonthAgoDateFormatted
-}
-
-const getConfirmedCasesByDate = (confirmedCases) => {
-    const datesOfConfirmedCases = confirmedCases.map(confirmedCase => confirmedCase.date.split("T").splice(0, 1).toString()).reverse()
-    const twoMonthAgoDate = getTwoMonthAgoDate()
-    let confirmedCasesByDate = {}
-    let caseCounter = 0;
-    datesOfConfirmedCases.forEach((date, i) => {
-        if (date === twoMonthAgoDate) {
-            return
-        } 
-        else {
-            caseCounter++;
-            if (date !== datesOfConfirmedCases[i + 1]) {
-                confirmedCasesByDate[date] = caseCounter
-                caseCounter = 0;
-            }
+const getConfirmedCasesDataObj = (confirmedCases) => {
+    const formatDateString = (date) =>  date.split("T").splice(0, 1).toString()
+    const confirmedCasesReversed = confirmedCases.reverse()
+    let confirmedCasesDataObj = {byDistricts: {}, byDate: {}}
+    confirmedCasesReversed
+                .map(confirmedCase => formatDateString(confirmedCase.date))
+                .forEach(date => { confirmedCasesDataObj["byDate"][date] =  0 })
+    confirmedCasesReversed
+                .map(confirmedCase => confirmedCase.healthCareDistrict)
+                .forEach(district => { confirmedCasesDataObj["byDistricts"][district] =  0 })
+    confirmedCasesReversed.forEach((confirmedCase, i) => {
+        const formattedConfirmedCaseDate = formatDateString(confirmedCase.date)
+        if (formattedConfirmedCaseDate) {
+            confirmedCasesDataObj["byDate"][formattedConfirmedCaseDate] += 1
         }
+        confirmedCasesDataObj["byDistricts"][confirmedCase.healthCareDistrict] += 1
     })
-    return confirmedCasesByDate
+    return confirmedCasesDataObj
 }
 
-const ctx = document.querySelector('#chart').getContext('2d'); // context of the chart describing element
-
-async function getData () {
+async function getDataObj () {
     try {
         const response = await fetch('https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData/v2');
         if (!response.ok) throw new Error('jokin meni pieleen');
         const formattedResponse = await response.json();
         const confirmedCases = formattedResponse.confirmed
-        const confirmedCasesByDate = getConfirmedCasesByDate(confirmedCases)
-        const chart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: Object.keys(confirmedCasesByDate).reverse(),
-                datasets: [{
-                    label: 'Confirmed cases in Finland for the past 2 months',
-                    fill: false,
-                    data: Object.values(confirmedCasesByDate).reverse(),
-                    backgroundColor: "red",
-                    borderColor: "red",
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        scaleLabel: {
-                            display: true,
-                            labelString: "Confirmed cases"
-                        }
-                    }],
-                    xAxes: [{
-                        scaleLabel: {
-                            display: true,
-                            labelString: "Time"
-                        }
-                    }]
-                }
-            }
-        })
+        const confirmedCasesDataObj = getConfirmedCasesDataObj(confirmedCases)
+        return confirmedCasesDataObj
     } catch (error) {
         console.log(error)
     }
 }
 
-getData()
+const lineChartCtx = document.querySelector('#lineChart').getContext('2d'); // context of the chart describing element
+const barChartCtx = document.querySelector('#barChart').getContext('2d');
+
+const chartOptions = {
+    scales: {
+        yAxes: [{
+            scaleLabel: {
+                display: true,
+                labelString: "Vahvistetut tapaukset"
+            }
+        }]
+    }
+}
+
+const barColors = ["#10316b", "#000000", "#e25822", "#ececeb", "#f6f578", "#f6d743", "#649d66", "#06623b", "#10316b", "#000000", "#e25822", "#ececeb", "#f6f578", "#f6d743", "#649d66", "#06623b", "#10316b", "#000000", "#e25822", "#ececeb", "#f6f578", "#f6d743"]  
+
+const drawCharts = (dataObj) => {
+    new Chart(lineChartCtx, {
+        type: 'line',
+        data: {
+            labels: Object.keys(dataObj["byDate"]).reverse(),
+            datasets: [{
+                label: 'Vahvistetut tapaukset tänä päivänä',
+                fill: false,
+                data: Object.values(dataObj["byDate"]).reverse(),
+                borderColor: "#10316b",
+                borderWidth: 1
+            }]
+        },
+        options: chartOptions
+    })
+    new Chart(barChartCtx, {
+        type: 'bar',
+        data: {
+            labels: Object.keys(dataObj["byDistricts"]),
+            datasets: [{
+                label: 'Vahvistetut tapaukset tässä sairaanhoitopiirissä',
+                fill: false,
+                data: Object.values(dataObj["byDistricts"]),
+                backgroundColor: barColors,
+                borderColor: barColors,
+                borderWidth: 1
+            }]
+        },
+        options: chartOptions
+    })
+}
+
+getDataObj().then(dataObj => drawCharts(dataObj))
